5c947c11a47c378b1c2a84560a7fa90ab68f466e
GugaSantos <guga.santos.guga@gmail.com>
2022-05-28 16:38:36 -0300

    Updates

---
+++ precache-manifest.e77483a52dc9e097fec10c1f4237c03c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdaf3881677da30b1fe610655dcd478b",
+    "url": "/TrybeWallet/index.html"
+  },
+  {
+    "revision": "b5d610303ec9bc771b10",
+    "url": "/TrybeWallet/static/css/main.02499b5e.chunk.css"
+  },
+  {
+    "revision": "6a280d82ccdc2d0b714c",
+    "url": "/TrybeWallet/static/js/2.e7d92441.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TrybeWallet/static/js/2.e7d92441.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5d610303ec9bc771b10",
+    "url": "/TrybeWallet/static/js/main.712b0cbe.chunk.js"
+  },
+  {
+    "revision": "7064498208332b0843ee",
+    "url": "/TrybeWallet/static/js/runtime-main.e31e136f.js"
+  },
+  {
+    "revision": "2b8540b23b07bdfa15fa168de38f79a6",
+    "url": "/TrybeWallet/static/media/image01.2b8540b2.svg"
+  },
+  {
+    "revision": "d50ea9b3eb0f75e075c2c4d107ea9fd1",
+    "url": "/TrybeWallet/static/media/purchases.d50ea9b3.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c57d79fed29e47b96f8f77c10e921ae6.js"
+  "/TrybeWallet/precache-manifest.e77483a52dc9e097fec10c1f4237c03c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TrybeWallet/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
